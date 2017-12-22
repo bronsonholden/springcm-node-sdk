1d3f97682ea42d75d0edf362fd03dec445aec73b
Paul Holden <pholden@stria.com>
2017-12-22 09:08:09 -0800

    Rework auth login

--- test/download.js
+++ test/download.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const SpringCM = require('../index');
 
-SpringCM.auth.uatna11(process.env.SPRINGCM_CLIENT_ID, process.env.SPRINGCM_CLIENT_SECRET, (err, token) => {
+SpringCM.auth.login(process.env.SPRINGCM_DATACENTER, process.env.SPRINGCM_CLIENT_ID, process.env.SPRINGCM_CLIENT_SECRET, (err, token) => {
 	if (err) {
 		return console.log('Auth error: ', err);
 	}
