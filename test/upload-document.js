a665b8da6a9eaa2618baffafb6e55d9ba7d58d54
Paul Holden <pholden@stria.com>
2018-05-08 12:12:11 -0700

    More upload tests

--- test/upload-document.js
+++ test/upload-document.js
@@ -21,26 +21,158 @@ describe('get-folder', function () {
     springCm.close(done);
   });
 
-  it('upload test PDF (default name)', function (done) {
-    springCm.getFolder('/Trash', (err, folder) => {
-      expect(err).to.not.exist;
-      expect(folder).to.exist;
-      springCm.uploadDocument(folder, fs.createReadStream('./test/Test.pdf'), (err) => {
+  describe('file naming', function () {
+    it('default name', function (done) {
+      springCm.getFolder('/Trash', (err, folder) => {
         expect(err).to.not.exist;
-        done();
+        expect(folder).to.exist;
+        springCm.uploadDocument(folder, fs.createReadStream('./test/Test.pdf'), (err) => {
+          expect(err).to.not.exist;
+          done();
+        });
+      });
+    });
+
+    it('specified name', function (done) {
+      springCm.getFolder('/Trash', (err, folder) => {
+        expect(err).to.not.exist;
+        expect(folder).to.exist;
+        springCm.uploadDocument(folder, fs.createReadStream('./test/Test.pdf'), {
+          name: 'Test.pdf'
+        }, (err) => {
+          expect(err).to.not.exist;
+          done();
+        });
       });
     });
   });
 
-  it('upload test PDF (specify name)', function (done) {
-    springCm.getFolder('/Trash', (err, folder) => {
-      expect(err).to.not.exist;
-      expect(folder).to.exist;
-      springCm.uploadDocument(folder, fs.createReadStream('./test/Test.pdf'), {
-        name: 'Test.pdf'
-      }, (err) => {
+  describe('PDF', function () {
+    it('application/pdf', function (done) {
+      springCm.getFolder('/Trash', (err, folder) => {
         expect(err).to.not.exist;
-        done();
+        expect(folder).to.exist;
+        springCm.uploadDocument(folder, fs.createReadStream('./test/Test.pdf'), {
+          name: 'Test.pdf',
+          fileType: 'pdf'
+        }, (err) => {
+          expect(err).to.not.exist;
+          done();
+        });
+      });
+    });
+
+    it('application/octet-stream', function (done) {
+      springCm.getFolder('/Trash', (err, folder) => {
+        expect(err).to.not.exist;
+        expect(folder).to.exist;
+        springCm.uploadDocument(folder, fs.createReadStream('./test/Test.pdf'), {
+          name: 'Test.pdf'
+        }, (err) => {
+          expect(err).to.not.exist;
+          done();
+        });
+      });
+    });
+  });
+
+  describe('CSV', function () {
+    it('text/csv', function (done) {
+      springCm.getFolder('/Trash', (err, folder) => {
+        expect(err).to.not.exist;
+        expect(folder).to.exist;
+        springCm.uploadDocument(folder, fs.createReadStream('./test/Test.csv'), {
+          name: 'Test.csv',
+          fileType: 'csv'
+        }, (err) => {
+          expect(err).to.not.exist;
+          done();
+        });
+      });
+    });
+
+    it('application/octet-stream', function (done) {
+      springCm.getFolder('/Trash', (err, folder) => {
+        expect(err).to.not.exist;
+        expect(folder).to.exist;
+        springCm.uploadDocument(folder, fs.createReadStream('./test/Test.csv'), {
+          name: 'Test.csv'
+        }, (err) => {
+          expect(err).to.not.exist;
+          done();
+        });
+      });
+    });
+  });
+
+  describe('TXT', function () {
+    it('text/plain', function (done) {
+      springCm.getFolder('/Trash', (err, folder) => {
+        expect(err).to.not.exist;
+        expect(folder).to.exist;
+        springCm.uploadDocument(folder, fs.createReadStream('./test/Test.txt'), {
+          name: 'Test.txt',
+          fileType: 'txt'
+        }, (err) => {
+          expect(err).to.not.exist;
+          done();
+        });
+      });
+    });
+
+    it('application/octet-stream', function (done) {
+      springCm.getFolder('/Trash', (err, folder) => {
+        expect(err).to.not.exist;
+        expect(folder).to.exist;
+        springCm.uploadDocument(folder, fs.createReadStream('./test/Test.txt'), {
+          name: 'Test.txt'
+        }, (err) => {
+          expect(err).to.not.exist;
+          done();
+        });
+      });
+    });
+  });
+
+  describe('DOCX', function () {
+    it('application/octet-stream', function (done) {
+      springCm.getFolder('/Trash', (err, folder) => {
+        expect(err).to.not.exist;
+        expect(folder).to.exist;
+        springCm.uploadDocument(folder, fs.createReadStream('./test/Test.docx'), {
+          name: 'Test.docx'
+        }, (err) => {
+          expect(err).to.not.exist;
+          done();
+        });
+      });
+    });
+
+    it('application/octet-stream', function (done) {
+      springCm.getFolder('/Trash', (err, folder) => {
+        expect(err).to.not.exist;
+        expect(folder).to.exist;
+        springCm.uploadDocument(folder, fs.createReadStream('./test/Test.txt'), {
+          name: 'Test.txt'
+        }, (err) => {
+          expect(err).to.not.exist;
+          done();
+        });
+      });
+    });
+  });
+
+  describe('TIFF', function () {
+    it('application/octet-stream', function (done) {
+      springCm.getFolder('/Trash', (err, folder) => {
+        expect(err).to.not.exist;
+        expect(folder).to.exist;
+        springCm.uploadDocument(folder, fs.createReadStream('./test/Test.tiff'), {
+          name: 'Test.tiff'
+        }, (err) => {
+          expect(err).to.not.exist;
+          done();
+        });
       });
     });
   });
