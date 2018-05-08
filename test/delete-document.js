4c3aca73421d415f5536634a3d00e8ce84812d28
Paul Holden <pholden@stria.com>
2018-05-08 15:24:59 -0700

    Add deleteDocument, some fixes

---
+++ test/delete-document.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+const { describe, it, before, after } = require('mocha');
+const { expect } = require('chai');
+const SpringCM = require('../');
+const env = require('./env');
+
+describe('delete-document', function () {
+  var springCm, root;
+
+  this.timeout(10000);
+
+  before(function (done) {
+    springCm = new SpringCM(env);
+    springCm.connect((err) => {
+      expect(err).to.not.exist;
+      springCm.getFolder('/', (err, folder) => {
+        expect(err).to.not.exist;
+        expect(folder).to.exist;
+        root = folder;
+        done();
+      });
+    });
+  });
+
+  after(function (done) {
+    springCm.close(done);
+  });
+
+  it('delete document by path', function (done) {
+    springCm.uploadDocument(root, fs.createReadStream('./test/Test.pdf'), {
+      name: 'Test Delete 1.pdf',
+      fileType: 'pdf'
+    }, (err, doc) => {
+      expect(err).to.not.exist;
+      expect(doc).to.exist;
+      springCm.deleteDocument('/Test Delete 1.pdf', (err) => {
+        expect(err).to.not.exist;
+        done();
+      });
+    });
+  });
+
+  it('delete document by uid', function (done) {
+    springCm.uploadDocument(root, fs.createReadStream('./test/Test.pdf'), {
+      name: 'Test Delete 2.pdf',
+      fileType: 'pdf'
+    }, (err, doc) => {
+      expect(err).to.not.exist;
+      expect(doc).to.exist;
+      springCm.deleteDocument(doc.getUid(), (err) => {
+        expect(err).to.not.exist;
+        done();
+      });
+    });
+  });
+
+  it('delete document by reference', function (done) {
+    springCm.uploadDocument(root, fs.createReadStream('./test/Test.pdf'), {
+      name: 'Test Delete 3.pdf',
+      fileType: 'pdf'
+    }, (err, doc) => {
+      expect(err).to.not.exist;
+      expect(doc).to.exist;
+      springCm.deleteDocument(doc, (err) => {
+        expect(err).to.not.exist;
+        done();
+      });
+    });
+  });
+});
---
+++ test/springcm-document.js
@@ -0,0 +1,119 @@
+const { describe, it, before, after } = require('mocha');
+const moment = require('moment');
+const chai = require('chai');
+const expect = chai.expect;
+const SpringCM = require('../');
+const env = require('./env');
+
+chai.use(require('chai-moment'));
+chai.use(require('chai-url'));
+
+describe('springcm-document', function () {
+  var springCm;
+
+  this.timeout(10000);
+
+  before(function (done) {
+    springCm = new SpringCM(env);
+    springCm.connect((err) => {
+      expect(err).to.not.exist;
+      done();
+    });
+  });
+
+  after(function (done) {
+    springCm.close(done);
+  });
+
+  describe('document access', function () {
+    var doc;
+
+    before(function (done) {
+      springCm.getDocument('/Test.pdf', (err, d) => {
+        expect(err).to.not.exist;
+        expect(d).to.exist;
+        doc = d;
+        done();
+      });
+    });
+
+    // Test document in root directory on super admin REST API account for testing
+    it('has document full access', function (done) {
+      expect(doc.canSee()).to.be.true;
+      expect(doc.canRead()).to.be.true;
+      expect(doc.canWrite()).to.be.true;
+      expect(doc.canMove()).to.be.true;
+      expect(doc.canSetAccess()).to.be.true;
+      done();
+    });
+  });
+
+  describe('document created/updated date', function () {
+    var doc;
+
+    before(function (done) {
+      springCm.getDocument('/Test.pdf', (err, d) => {
+        expect(err).to.not.exist;
+        expect(d).to.exist;
+        doc = d;
+        done();
+      });
+    });
+
+    it('has create date', function (done) {
+      expect(doc.getCreateDate()).to.be.beforeMoment(moment());
+      done();
+    });
+
+    it('has update date', function (done) {
+      expect(doc.getUpdateDate()).to.be.beforeMoment(moment());
+      done();
+    });
+  });
+
+  describe('document created/updated user', function () {
+    var doc;
+
+    before(function (done) {
+      springCm.getDocument('/Test.pdf', (err, d) => {
+        expect(err).to.not.exist;
+        expect(d).to.exist;
+        doc = d;
+        done();
+      });
+    });
+
+    it('has created user', function (done) {
+      expect(doc.getCreateUser()).to.be.a('string');
+      done();
+    });
+
+    it('has updated user', function (done) {
+      expect(doc.getUpdateUser()).to.be.a('string');
+      done();
+    });
+  });
+
+  describe('document properties', function (done) {
+    var doc;
+
+    before(function (done) {
+      springCm.getDocument('/Test.pdf', (err, d) => {
+        expect(err).to.not.exist;
+        expect(d).to.exist;
+        doc = d;
+        done();
+      });
+    });
+
+    it('document name', function (done) {
+      expect(doc.getName()).to.be.a('string');
+      done();
+    });
+
+    it('document description', function (done) {
+      expect(doc.getDescription()).to.be.a('string');
+      done();
+    });
+  });
+});
